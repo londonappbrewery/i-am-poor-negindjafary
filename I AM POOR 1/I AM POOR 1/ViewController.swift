41de6d8647be756947526f95b6965948d054e08f
Negin <neginsongs@gmail.com>
2018-01-15 11:36:50 +0000

    submission 1 - new

---
+++ I AM POOR 1/I AM POOR 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I AM POOR 1
+//
+//  Created by Negin Djafari on 04/01/2018.
+//  Copyright © 2018 Negin Djafari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
